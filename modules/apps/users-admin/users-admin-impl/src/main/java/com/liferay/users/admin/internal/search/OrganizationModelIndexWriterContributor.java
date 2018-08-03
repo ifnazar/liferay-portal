9813a7340a5c87640701139c478adfcb724283d5
Igor Fabiano Nazar <ifnazar@gmail.com>
2018-08-03 15:53:10 -0300

    xxxx Migration of Organization entity indexing mechanism

---
+++ modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/OrganizationModelIndexWriterContributor.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.internal.search;
+
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Igor Fabiano Nazar
+ * @author Luan Maoski
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.portal.kernel.model.Organization",
+	service = ModelIndexerWriterContributor.class
+)
+public class OrganizationModelIndexWriterContributor
+	implements ModelIndexerWriterContributor<Organization> {
+
+	//TODO need test
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+
+		batchIndexingActionable.setPerformActionMethod(
+			(Organization organization) -> {
+				Document document =
+					modelIndexerWriterDocumentHelper.getDocument(organization);
+
+				batchIndexingActionable.addDocuments(document);
+			});
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return null;
+	}
+
+	@Override
+	public long getCompanyId(Organization organization) {
+		return organization.getCompanyId();
+	}
+
+	@Override
+	public void modelIndexed(Organization organization) {
+	}
+
+	@Reference
+	protected DynamicQueryBatchIndexingActionableFactory
+		dynamicQueryBatchIndexingActionableFactory;
+
+}
---
+++ modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/OrganizationModelPreFilterContributor.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.internal.search;
+
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
+import com.liferay.portal.search.spi.model.registrar.ModelSearchSettings;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Igor Fabiano Nazar
+ * @author Luan Maoski
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.portal.kernel.model.Organization",
+	service = ModelPreFilterContributor.class
+)
+public class OrganizationModelPreFilterContributor
+	implements ModelPreFilterContributor {
+
+	@Override
+	public void contribute(
+		BooleanFilter booleanFilter, ModelSearchSettings modelSearchSettings,
+		SearchContext searchContext) {
+
+		//TODO
+	}
+
+}
