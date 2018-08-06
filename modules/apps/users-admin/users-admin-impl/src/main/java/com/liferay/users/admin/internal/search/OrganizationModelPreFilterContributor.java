8eba1fd762be4827ad5fdd687c5dd5fe59d84675
Igor Fabiano Nazar <ifnazar@gmail.com>
2018-08-06 15:41:01 -0300

    xxx OrganizationModelPreFilterContributor

--- modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/OrganizationModelPreFilterContributor.java
+++ modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/OrganizationModelPreFilterContributor.java
@@ -14,10 +14,28 @@
 
 package com.liferay.users.admin.internal.search;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.OrganizationConstants;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.TermQuery;
+import com.liferay.portal.kernel.search.WildcardQuery;
 import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.QueryFilter;
+import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.search.generic.TermQueryImpl;
+import com.liferay.portal.kernel.search.generic.WildcardQueryImpl;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
 import com.liferay.portal.search.spi.model.registrar.ModelSearchSettings;
+
+import java.util.LinkedHashMap;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -34,11 +52,77 @@ public class OrganizationModelPreFilterContributor
 	implements ModelPreFilterContributor {
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public void contribute(
-		BooleanFilter booleanFilter, ModelSearchSettings modelSearchSettings,
-		SearchContext searchContext) {
+		BooleanFilter contextBooleanFilter,
+		ModelSearchSettings modelSearchSettings, SearchContext searchContext) {
 
-		//TODO
+		LinkedHashMap<String, Object> params =
+			(LinkedHashMap<String, Object>)searchContext.getAttribute("params");
+
+		if (params == null) {
+			return;
+		}
+
+		List<Long> excludedOrganizationIds = (List<Long>)params.get(
+			"excludedOrganizationIds");
+
+		if (ListUtil.isNotEmpty(excludedOrganizationIds)) {
+			TermsFilter termsFilter = new TermsFilter("organizationId");
+
+			termsFilter.addValues(
+				ArrayUtil.toStringArray(
+					excludedOrganizationIds.toArray(
+						new Long[excludedOrganizationIds.size()])));
+
+			contextBooleanFilter.add(termsFilter, BooleanClauseOccur.MUST_NOT);
+		}
+
+		List<Organization> organizationsTree = (List<Organization>)params.get(
+			"organizationsTree");
+
+		if (organizationsTree != null) {
+			BooleanFilter booleanFilter = new BooleanFilter();
+
+			if (organizationsTree.isEmpty()) {
+				TermQuery termQuery = new TermQueryImpl(
+					Field.TREE_PATH, StringPool.BLANK);
+
+				booleanFilter.add(new QueryFilter(termQuery));
+			}
+
+			for (Organization organization : organizationsTree) {
+				String treePath;
+
+				try {
+					treePath = organization.buildTreePath();
+				}
+				catch (PortalException pe) {
+
+					// TODO  RuntimeException ??
+
+					throw new RuntimeException(pe);
+				}
+
+				WildcardQuery wildcardQuery = new WildcardQueryImpl(
+					Field.TREE_PATH, treePath);
+
+				booleanFilter.add(new QueryFilter(wildcardQuery));
+			}
+
+			contextBooleanFilter.add(booleanFilter, BooleanClauseOccur.MUST);
+		}
+		else {
+			long parentOrganizationId = GetterUtil.getLong(
+				searchContext.getAttribute("parentOrganizationId"));
+
+			if (parentOrganizationId !=
+					OrganizationConstants.ANY_PARENT_ORGANIZATION_ID) {
+
+				contextBooleanFilter.addRequiredTerm(
+					"parentOrganizationId", parentOrganizationId);
+			}
+		}
 	}
 
 }
