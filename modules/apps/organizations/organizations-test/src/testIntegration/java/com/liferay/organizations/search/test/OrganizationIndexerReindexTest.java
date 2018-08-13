996bf36efb6a16464e2abcc672c541a0d6504372
Igor Fabiano Nazar <igor.nazar@liferay.com>
2018-08-13 13:12:58 -0300

    LPS-87831 organizations-test: Add integration test to check the reindex for the Organization.
    https://issues.liferay.com/browse/LPS-87831

---
+++ modules/apps/organizations/organizations-test/src/testIntegration/java/com/liferay/organizations/search/test/OrganizationIndexerReindexTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.organizations.search.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerTestRule;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Igor Fabiano Nazar
+ * @author Luan Maoski
+ */
+@RunWith(Arquillian.class)
+public class OrganizationIndexerReindexTest
+	extends BaseOrganizationIndexerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerTestRule.INSTANCE,
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+
+		super.setGroup(organizationFixture.addGroup());
+	}
+
+	@Test
+	public void testIndexedFields() throws Exception {
+		String organizationName = "abcd efgh";
+		String countryName = "united-states";
+		String regionName = "Alabama";
+
+		Organization organization = organizationFixture.createAnOrganization(
+			organizationName, countryName, regionName);
+
+		organizationIndexerFixture.searchOnlyOne(organizationName);
+		organizationIndexerFixture.reindex(organization.getCompanyId());
+		organizationIndexerFixture.searchOnlyOne(organizationName);
+	}
+
+}
