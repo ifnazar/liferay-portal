cbf19967685afa69e930704e289f5cdcc09ca863
Igor Fabiano Nazar <igor.nazar@liferay.com>
2018-12-07 13:54:01 -0200

    LPS-84620 journal-test: Add test to reindex DDMStructure for JournalArticle
    https://issues.liferay.com/browse/LPS-84620

---
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticleDDMStructureFixture.java
@@ -0,0 +1,112 @@
+package com.liferay.journal.search.test;
+
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestUtil;
+import com.liferay.dynamic.data.mapping.test.util.DDMTemplateTestUtil;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.journal.test.util.search.JournalArticleBlueprint;
+import com.liferay.journal.test.util.search.JournalArticleContent;
+import com.liferay.journal.test.util.search.JournalArticleSearchFixture;
+import com.liferay.journal.test.util.search.JournalArticleTitle;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * @author Igor Fabiano Nazar
+ * @author Lucas Marques de Paula
+ */
+public class JournalArticleDDMStructureFixture {
+
+	public JournalArticleDDMStructureFixture(
+		Group group, JournalArticleLocalService journalArticleLocalService) {
+
+		_group = group;
+
+		_journalArticleSearchFixture = new JournalArticleSearchFixture(
+			journalArticleLocalService);
+	}
+
+	public DDMStructure createStructureWithJournalArticle(
+			String title, Locale locale)
+		throws Exception, PortalException {
+
+		JournalArticleBlueprint journalArticleBlueprint =
+			_createJournalArticleBlueprint(title, null, locale);
+
+		DDMForm ddmForm = DDMStructureTestUtil.getSampleDDMForm(
+			new Locale[] {locale}, locale);
+
+		DDMStructure ddmStructure = DDMStructureTestUtil.addStructure(
+			_group.getGroupId(), JournalArticle.class.getName(), ddmForm,
+			locale);
+
+		_ddmStructures.add(ddmStructure);
+
+		DDMTemplate ddmTemplate = DDMTemplateTestUtil.addTemplate(
+			_group.getGroupId(), ddmStructure.getStructureId(),
+			PortalUtil.getClassNameId(JournalArticle.class));
+
+		getTemplates().add(ddmTemplate);
+
+		String ddmStructureKey = ddmStructure.getStructureKey();
+		String ddmTemplateKey = ddmTemplate.getTemplateKey();
+
+		JournalArticle journalArticle = _journalArticleSearchFixture.addArticle(
+			journalArticleBlueprint, ddmStructureKey, ddmTemplateKey);
+
+		getJournalArticles().add(journalArticle);
+
+		return ddmStructure;
+	}
+
+	public List<JournalArticle> getJournalArticles() {
+		return _journalArticleSearchFixture.getJournalArticles();
+	}
+
+	public List<DDMStructure> getStructures() {
+		return _ddmStructures;
+	}
+
+	public List<DDMTemplate> getTemplates() {
+		return _ddmTemplates;
+	}
+
+	private JournalArticleBlueprint _createJournalArticleBlueprint(
+		String title, String content, Locale locale) {
+
+		JournalArticleBlueprint journalArticleBlueprint =
+			new JournalArticleBlueprint() {
+				{
+					groupId = _group.getGroupId();
+					journalArticleContent = new JournalArticleContent() {
+						{
+							name = "content";
+							defaultLocale = locale;
+							put(locale, content);
+						}
+					};
+					journalArticleTitle = new JournalArticleTitle() {
+						{
+							put(locale, title);
+						}
+					};
+				}
+			};
+
+		return journalArticleBlueprint;
+	}
+
+	private final List<DDMStructure> _ddmStructures = new ArrayList<>();
+	private final List<DDMTemplate> _ddmTemplates = new ArrayList<>();
+	private final Group _group;
+	private final JournalArticleSearchFixture _journalArticleSearchFixture;
+
+}
